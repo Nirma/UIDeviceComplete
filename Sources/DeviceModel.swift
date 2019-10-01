c4b98552c30e0ba557c694a98d0203c8afe962df
Casey Law <sacred0214@gmail.com>
2019-10-01 12:41:11 +0800

    import Foundation to resolve build issue with swift package manager

--- Sources/DeviceModel.swift
+++ Sources/DeviceModel.swift
@@ -21,6 +21,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+import Foundation
+
 public enum DeviceModel: CaseIterable {
 
     case iPhone4, iPhone4S
