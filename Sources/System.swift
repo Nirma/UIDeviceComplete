94f10445ce7444f66c4216cfda3ba73578597960
Dylan Mace <dtmace2@gmail.com>
2022-08-08 14:48:11 -0400

    remove if let statement

--- Sources/System.swift
+++ Sources/System.swift
@@ -27,16 +27,15 @@ class System {
     static var name: String? {
         var systemInfo = utsname()
         uname(&systemInfo)
-        if let identifier = withUnsafePointer(to: &systemInfo.machine, {
+        let identifier = withUnsafePointer(to: &systemInfo.machine, {
             $0.withMemoryRebound(to: CChar.self, capacity: Int(_SYS_NAMELEN)) {
                 ptr in String(cString: ptr)
-            }}) {
-            // Simulator Check
-            if identifier == "x86_64" || identifier == "i386" || identifier == "arm64" {
-                return ProcessInfo.processInfo.environment["SIMULATOR_MODEL_IDENTIFIER"]
             }
-            return identifier
+        })
+        // Simulator Check
+        if identifier == "x86_64" || identifier == "i386" || identifier == "arm64" {
+            return ProcessInfo.processInfo.environment["SIMULATOR_MODEL_IDENTIFIER"]
         }
-        return nil
+        return identifier
     }
 }
